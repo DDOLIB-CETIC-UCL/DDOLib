d555f6d23502a18719bd6930bca02183f2c4dc46
Germeau Fabian <fabian.germeau@cetic.be>
2025-03-14 11:48:27 +0100

    Tried to improve and fix this example, couldn't

---
+++ src/main/java/org/ddolib/ddo/examples/alp/ALPProblem.java
@@ -0,0 +1,169 @@
+package org.ddolib.ddo.examples.alp;
+
+import org.ddolib.ddo.core.Decision;
+import org.ddolib.ddo.core.Problem;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+
+public class ALPProblem implements Problem<ALPState> {
+    public ALPInstance instance;
+    public ArrayList<ArrayList<Integer>> next;
+    int[] minSeparationTo;
+
+    public static final int DUMMY = -1;
+
+    public ALPProblem(ALPInstance instance){
+        this.instance = instance;
+        next = new ArrayList<>();
+        // Minimal time between a "no_class" aircraft and "class" aircraft.
+        minSeparationTo = new int[instance.nbClasses];
+
+        for(int i = 0; i < instance.nbClasses; i++)
+            next.add(new ArrayList<>(List.of(0)));
+        Arrays.fill(minSeparationTo,Integer.MAX_VALUE);
+
+        for(int i = instance.nbAircraft-1; i >= 0; i--){
+            next.get(instance.classes[i]).add(i);
+        }
+
+        for(int i = 0; i < instance.nbClasses; i++){
+            for(int j = 0; j < instance.nbClasses; j++){
+                minSeparationTo[j] = Math.min(minSeparationTo[j],instance.separation[i][j]);
+            }
+        }
+    }
+
+    /** Returns the arrival time of an aircraft based on runway state.
+     *
+     * @param runwayStates The array of runway states.
+     * @param aircraft The aircraft for which we want to know the arrival time.
+     * @param runway The runway on which the aircraft is supposed to land.
+     * @return The arrival (landing) time.
+     */
+    public int getArrivalTime(RunwayState[] runwayStates, int aircraft, int runway){
+        if(runwayStates[runway].prevTime == 0 & runwayStates[runway].prevClass == DUMMY){
+            return instance.target[aircraft];
+        } else if(runwayStates[runway].prevClass == DUMMY){
+            return Math.max(instance.target[aircraft],
+                    runwayStates[runway].prevTime + minSeparationTo[instance.classes[aircraft]]);
+        } else {
+            return Math.max(instance.target[aircraft],
+                    runwayStates[runway].prevTime +
+                            instance.separation[runwayStates[runway].prevClass][instance.classes[aircraft]]);
+        }
+    }
+
+    /** Formats the ALPDecision as an Integer
+     *
+     * @param decision The decision.
+     * @return The formated decision.
+     */
+    public int toDecision(ALPDecision decision){
+        return decision.aircraftClass + instance.nbClasses*decision.runway;
+    }
+
+    /** Restore a decision object from its Integer form.
+     *
+     * @param value THe formatted Integer form of the decision.
+     * @return An ALPDecision object.
+     */
+    public ALPDecision fromDecision(int value){
+        return new ALPDecision(
+                value % instance.nbClasses,
+                value/instance.nbClasses
+        );
+    }
+
+    @Override
+    public int nbVars() {
+        return instance.nbAircraft;
+    }
+
+    @Override
+    public ALPState initialState() {
+        int[] remaining = new int[instance.nbClasses];
+        Arrays.fill(remaining,0);
+        for(int i = 0; i < instance.nbAircraft; i++)
+            remaining[instance.classes[i]] += 1;
+        RunwayState[] runwayStates = new RunwayState[instance.nbRunways];
+        Arrays.fill(runwayStates, new RunwayState(DUMMY,0));
+        return new ALPState(remaining, runwayStates);
+    }
+
+    @Override
+    public int initialValue() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<Integer> domain(ALPState state, int var) {
+        int totRemaining = 0;
+        HashSet<RunwayState> used = new HashSet<>();
+        ArrayList<Integer> decisions = new ArrayList<>();
+        int[] remainingAircraftOfClass = state.remainingAircraftOfClass;
+
+        for (int i = 0; i < remainingAircraftOfClass.length; i++){       // For each class
+            if(remainingAircraftOfClass[i] > 0){                         // If there still are aircraft to land.
+                // Get the earliest aircraft in the queue. (sorted backwardly)
+                int aircraft = next.get(i).get(state.remainingAircraftOfClass[i]);
+
+                used.clear();
+                for(int runway: IntStream.range(0,instance.nbRunways).toArray()){ // For each runway, try to find at least one suitable runway.
+                    int arrival = getArrivalTime(state.runwayStates, aircraft, runway);
+                    if(arrival <= instance.latest[aircraft]) {
+                        decisions.add(toDecision(new ALPDecision(i,runway)));
+                        used.add(state.runwayStates[runway]);
+                    }
+                }
+
+                if(used.isEmpty()){
+                    // This aircraft will never be able to land.
+                    return Collections.emptyIterator();
+                }
+
+            }
+            totRemaining += remainingAircraftOfClass[i];
+        }
+
+        if(totRemaining == 0){
+            return Collections.singletonList(DUMMY).iterator();
+        } else {
+            return decisions.iterator();
+        }
+    }
+
+    @Override
+    public ALPState transition(ALPState state, Decision decision) {
+        if(decision.val() == DUMMY){
+            return new ALPState(state);
+        } else {
+            ALPDecision alpDecision = fromDecision(decision.val());
+            int aircraftClass = alpDecision.aircraftClass;
+            int runway = alpDecision.runway;
+            int aircraft = next.get(aircraftClass).get(state.remainingAircraftOfClass[aircraftClass]);
+            ALPState nextState = new ALPState(state);
+            nextState.remainingAircraftOfClass[aircraftClass] -= 1;
+            nextState.runwayStates[runway].prevClass = aircraftClass;
+            nextState.runwayStates[runway].prevTime = getArrivalTime(state.runwayStates, aircraft, runway);
+
+            Arrays.sort(nextState.runwayStates);
+            return nextState;
+        }
+    }
+
+    @Override
+    public int transitionCost(ALPState state, Decision decision) {
+        if(decision.val() == DUMMY) {
+            return 0;
+        } else {
+            ALPDecision alpDecision = fromDecision(decision.val());
+            int aircraftClass = alpDecision.aircraftClass;
+            int aircraft = next.get(aircraftClass).get(state.remainingAircraftOfClass[aircraftClass]);
+            return -(getArrivalTime(state.runwayStates, aircraft, alpDecision.runway) - instance.target[aircraft]);
+        }
+    }
+}
+
+
---
+++ src/main/java/org/ddolib/ddo/examples/alp/ALPRelax.java
@@ -0,0 +1,64 @@
+package org.ddolib.ddo.examples.alp;
+
+import org.ddolib.ddo.core.Decision;
+import org.ddolib.ddo.core.Relaxation;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ALPRelax implements Relaxation<ALPState> {
+
+    ALPProblem problem;
+
+    public ALPRelax(ALPProblem problem){
+        this.problem = problem;
+    }
+
+    @Override
+    public ALPState mergeStates(Iterator<ALPState> states) {
+        int[] remainingAircraft = new int[problem.instance.nbClasses];
+        Arrays.fill(remainingAircraft, Integer.MIN_VALUE);
+        RunwayState[] runwayStates = new RunwayState[problem.instance.nbRunways];
+        Arrays.fill(runwayStates, new RunwayState(ALPProblem.DUMMY, Integer.MAX_VALUE));
+
+        // Set the remaining nb of aircraft (for each class) of the merged state as the minimal value of each merged states.
+        // Set the previous time of each runway of the merged state as the minimal value of each merged states.
+        while (states.hasNext()) {
+            ALPState s = states.next();
+            for (int i = 0; i < remainingAircraft.length; i++) {
+                remainingAircraft[i] = Math.max(remainingAircraft[i], s.remainingAircraftOfClass[i]);
+            }
+            for (int i = 0; i < runwayStates.length; i++) {
+                runwayStates[i].prevTime = Math.min(s.runwayStates[i].prevTime, runwayStates[i].prevTime);
+            }
+        }
+
+        return new ALPState(remainingAircraft, runwayStates);
+    }
+
+    @Override
+    public int relaxEdge(ALPState from, ALPState to, ALPState merged, Decision d, int cost) {
+        return cost;
+    }
+
+    @Override
+    public int fastUpperBound(ALPState state, Set<Integer> variables) {
+        int sum = 0;
+        ALPInstance inst = problem.instance;
+
+        for(int aircraft: variables){
+            int bestRunwayTardiness = Integer.MAX_VALUE;
+            for(int runway = 0; runway < state.runwayStates.length; runway++) {
+                int arrivalTime = problem.getArrivalTime(state.runwayStates, aircraft, runway);
+                if(arrivalTime - inst.latest[aircraft] <= 0) {  // If one aircraft can not land, the fub is maxValue
+                    int estimatedTardiness = Math.max(0,problem.getArrivalTime(state.runwayStates, aircraft, runway) - inst.target[aircraft]);
+                    bestRunwayTardiness = Math.min(bestRunwayTardiness,estimatedTardiness);
+                }
+            }
+            if(bestRunwayTardiness == Integer.MAX_VALUE) return (bestRunwayTardiness);
+            sum += bestRunwayTardiness;
+        }
+        return (-sum);
+    }
+}
