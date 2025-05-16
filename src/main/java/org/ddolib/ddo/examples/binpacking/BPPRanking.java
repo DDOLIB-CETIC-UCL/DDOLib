a0e315d30c49695b716162243f05d73a950538eb
Germeau Fabian <fabian.germeau@cetic.be>
2025-05-16 11:23:58 +0200

    Add test, spot a problem with relaxation

--- src/main/java/org/ddolib/ddo/examples/binpacking/BPPRanking.java
+++ src/main/java/org/ddolib/ddo/examples/binpacking/BPPRanking.java
@@ -5,8 +5,8 @@ import org.ddolib.ddo.heuristics.StateRanking;
 public class BPPRanking implements StateRanking<BPPState> {
     @Override
     public int compare(BPPState o1, BPPState o2) {
-        int fullBinComparison = Integer.compare(o1.bins.size(), o2.bins.size());
-        if(fullBinComparison != 0) return fullBinComparison;
+        int nbBinsComparison = Integer.compare(o1.bins.size(), o2.bins.size());
+        if(nbBinsComparison != 0) return nbBinsComparison;
 
         int totRemainingSpaceO1 = o1.bins.stream().map(Bin::remainingSpace).reduce(0, Integer::sum);
         int totRemainingSpaceO2 = o2.bins.stream().map(Bin::remainingSpace).reduce(0, Integer::sum);
