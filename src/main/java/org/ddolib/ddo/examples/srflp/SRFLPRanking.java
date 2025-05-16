d33b99f722df73d961dd081a0946b3a2ceca298b
qme <quentin.meurisse@cetic.be>
2025-05-16 14:59:49 +0200

    implement ranking

--- src/main/java/org/ddolib/ddo/examples/srflp/SRFLPRanking.java
+++ src/main/java/org/ddolib/ddo/examples/srflp/SRFLPRanking.java
@@ -6,6 +6,8 @@ public class SRFLPRanking implements StateRanking<SRFLPState> {
 
     @Override
     public int compare(SRFLPState o1, SRFLPState o2) {
-        return 0;
+        int total1 = o1.must().cardinality() + o1.maybe().cardinality();
+        int total2 = o2.must().cardinality() + o2.maybe().cardinality();
+        return Integer.compare(total1, total2);
     }
 }
