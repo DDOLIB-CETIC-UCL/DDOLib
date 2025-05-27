bcaf813ea01bfef4bb182d3ea3909e5a760858d6
qme <quentin.meurisse@cetic.be>
2025-05-27 14:23:27 +0200

    remove some intellij warning, add some doc and reformat

--- src/main/java/org/ddolib/ddo/examples/tsptw/TSPTWDominance.java
+++ src/main/java/org/ddolib/ddo/examples/tsptw/TSPTWDominance.java
@@ -6,14 +6,14 @@ import java.util.BitSet;
 
 /**
  * Dominance class for the TSPTW problem.
- * If for two states, the position is the same and the must visit set is the same,
+ * If for two states, the position is the same and the {@code mustVisit} set is the same,
  * then we can only keep the state with the lowest time.
  */
-public class TSPTWDominance implements Dominance<TSPTWState,TSPTWDominanceKey> {
+public class TSPTWDominance implements Dominance<TSPTWState, TSPTWDominanceKey> {
 
     @Override
     public TSPTWDominanceKey getKey(TSPTWState state) {
-        return new TSPTWDominanceKey(state.position(),state.mustVisit());
+        return new TSPTWDominanceKey(state.position(), state.mustVisit());
     }
 
     @Override
