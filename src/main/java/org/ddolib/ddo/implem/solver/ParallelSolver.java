06e4a299e22cfa127234a1590adb9ee8d4a69258
pschaus <pschaus@gmail.com>
2025-05-30 11:59:55 +0200

    wip frontier cutset

--- src/main/java/org/ddolib/ddo/implem/solver/ParallelSolver.java
+++ src/main/java/org/ddolib/ddo/implem/solver/ParallelSolver.java
@@ -193,7 +193,8 @@ public final class ParallelSolver<T,K> implements Solver {
             sub,
             width,
             shared.dominance,
-            bestLB
+            bestLB,
+            CutSetType.LastExactLayer
         );
 
         mdd.compile(compilation);
@@ -213,7 +214,8 @@ public final class ParallelSolver<T,K> implements Solver {
             sub,
             width,
             shared.dominance,
-            bestLB
+            bestLB,
+            CutSetType.LastExactLayer
         );
         mdd.compile(compilation);
         if (mdd.isExact()) {
