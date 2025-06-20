9004faee04d53cc17b56220797c4cdf7345511dd
qme <quentin.meurisse@cetic.be>
2025-06-20 14:42:07 +0200

    add javadoc

--- src/test/java/org/ddolib/ddo/testbench/ProblemTestBench.java
+++ src/test/java/org/ddolib/ddo/testbench/ProblemTestBench.java
@@ -13,18 +13,46 @@ import static org.ddolib.ddo.implem.solver.Solvers.sequentialSolver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
+/**
+ * Abstract class to generate tests on implementations of {@link Problem}. The user need to implement an instance
+ * generator and a {@link SolverConfig}.
+ *
+ * @param <T> The type of states.
+ * @param <K> The type of dominance keys.
+ * @param <P> The type of problem to test.
+ */
 public abstract class ProblemTestBench<T, K, P extends Problem<T>> {
 
+    /**
+     * List of problem used for tests.
+     */
     protected final List<P> problems;
 
+    /**
+     * Generates {@link Problem} instances to test.
+     *
+     * @return A list of problems used for tests.
+     */
     abstract protected List<P> generateProblems();
 
+    /**
+     * Given a problem, returns the solver's inputs (relaxation, dominance, frontier,...).
+     *
+     * @param problem A problem to solve during the tests.
+     * @return The solver's inputs (relaxation, dominance, frontier,...).
+     */
     abstract protected SolverConfig<T, K> configSolver(P problem);
 
     public ProblemTestBench() {
         problems = generateProblems();
     }
 
+    /**
+     * Test if the exact mdd generated for the input problem lead to optimal solution.
+     *
+     * @param problem The instance to test.
+     */
     protected void testTransitionModel(P problem) {
         SolverConfig<T, K> config = configSolver(problem);
         Solver solver = sequentialSolver(problem, config.relax(), config.varh(), config.ranking(), config.width(), config.frontier());
@@ -32,8 +60,15 @@ public abstract class ProblemTestBench<T, K, P extends Problem<T>> {
         assertEquals(problem.optimalValue().get(), solver.bestValue().get(), 1e-10);
     }
 
+    /**
+     * Test if the fast upper bound is an upper bound for the root node and if the compilation with the fast upper
+     * bound enabled lead to the optimal solution.
+     *
+     * @param problem The instance to test.
+     */
     protected void testFub(P problem) {
         SolverConfig<T, K> config = configSolver(problem);
+        Solver solver = sequentialSolver(problem, config.relax(), config.varh(), config.ranking(), config.width(), config.frontier());
 
         HashSet<Integer> vars = new HashSet<>();
         for (int i = 0; i < problem.nbVars(); i++) {
@@ -45,8 +80,16 @@ public abstract class ProblemTestBench<T, K, P extends Problem<T>> {
                 String.format("Upper bound %.2f is not bigger than the expected optimal solution %.2f",
                         rub,
                         problem.optimalValue().get()));
+
+        solver.maximize();
+        assertEquals(problem.optimalValue().get(), solver.bestValue().get(), 1e-10);
     }
 
+    /**
+     * Test if the model with relaxation enabled lead to the optimal solution.
+     *
+     * @param problem The instance to test.
+     */
     protected void testRelaxation(P problem) {
         SolverConfig<T, K> config = configSolver(problem);
 
@@ -60,6 +103,11 @@ public abstract class ProblemTestBench<T, K, P extends Problem<T>> {
         }
     }
 
+    /**
+     * Test if the model with the dominance enabled lead to the optimal solution.
+     *
+     * @param problem The instance to test.
+     */
     protected void testDominance(P problem) {
         SolverConfig<T, K> config = configSolver(problem);
         Solver solver = sequentialSolver(problem, config.relax(), config.varh(), config.ranking(), config.width(),
@@ -69,6 +117,11 @@ public abstract class ProblemTestBench<T, K, P extends Problem<T>> {
         assertEquals(problem.optimalValue().get(), solver.bestValue().get(), 1e-10);
     }
 
+    /**
+     * Generates all the tests for all the generated instances.
+     *
+     * @return A stream of tests over all the generated instances.
+     */
     public Stream<DynamicTest> generateTests() {
         Stream<DynamicTest> modelTests = problems.stream().map(p ->
                 DynamicTest.dynamicTest(String.format("Model for %s", p.toString()), () -> testTransitionModel(p))
--- src/test/java/org/ddolib/ddo/testbench/SolverConfig.java
+++ src/test/java/org/ddolib/ddo/testbench/SolverConfig.java
@@ -7,6 +7,28 @@ import org.ddolib.ddo.heuristics.VariableHeuristic;
 import org.ddolib.ddo.heuristics.WidthHeuristic;
 import org.ddolib.ddo.implem.dominance.DominanceChecker;
 
+/**
+ * Class containing the input of {@link org.ddolib.ddo.implem.solver.SequentialSolver}.
+ *
+ * @param relax     A suitable relaxation for the problem we want to maximize. If set to {@code null}, the relaxation
+ *                  will not be tested.
+ * @param varh      A heuristic to choose the next variable to branch on when developing a DD.
+ * @param ranking   A heuristic to identify the most promising nodes.
+ * @param width     A heuristic to choose the maximum width of the DD you compile.
+ * @param frontier  The set of nodes that must still be explored before
+ *                  the problem can be considered 'solved'.
+ *                  <p>
+ *                  # Note:
+ *                  This fringe orders the nodes by upper bound (so the highest ub is going
+ *                  to pop first). So, it is guaranteed that the upper bound of the first
+ *                  node being popped is an upper bound on the value reachable by exploring
+ *                  any of the nodes remaining on the fringe. As a consequence, the
+ *                  exploration can be stopped as soon as a node with an ub &#8804; current best
+ *                  lower bound is popped.
+ * @param dominance The dominance object that will be used to prune the search space.
+ * @param <T>       The type of the states.
+ * @param <K>       The type of the dominance keys.
+ */
 public record SolverConfig<T, K>(
         Relaxation<T> relax,
         VariableHeuristic<T> varh,
