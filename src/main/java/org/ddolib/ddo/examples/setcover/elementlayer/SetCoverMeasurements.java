a4c920cd5f822ce18966ef2ca5b77359489b5554
BurlatsAlice <52482818+BurlatsAuguste@users.noreply.github.com>
2025-06-30 16:52:30 +0200

    change measurements interface for setcover

--- src/main/java/org/ddolib/ddo/examples/setcover/elementlayer/SetCoverMeasurements.java
+++ src/main/java/org/ddolib/ddo/examples/setcover/elementlayer/SetCoverMeasurements.java
@@ -9,7 +9,9 @@ import org.ddolib.ddo.implem.frontier.SimpleFrontier;
 import org.ddolib.ddo.implem.heuristics.FixedWidth;
 import org.ddolib.ddo.implem.solver.RelaxationSolver;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.List;
 
@@ -19,72 +21,62 @@ public class SetCoverMeasurements {
 
     public static void main(String[] args) throws IOException {
 
-        String[] files = {
-                "data/SetCover/1id_problem/tripode",
-                "data/SetCover/generated/n_6_b_5_d_5",
-                "data/SetCover/generated/n_10_b_8_d_3",
-                "data/SetCover/1id_problem/abilene",
-                "data/SetCover/1id_problem/ai3",
-                "data/SetCover/1id_problem/gblnet",
-                "data/SetCover/1id_problem/aarnet"};
+        // File dir = new File("./data/SetCover/1id_problem");
+        // FilenameFilter filter = (dir1, name) -> name.endsWith(".txt");
+        // File[] instances = dir.listFiles(filter);
 
-        String header = "Name;Solver;Seed;MaxWidth;Model;VarHeuristic;MergeStrategy;DistanceFunction;Time(s);Objective";
+        // String header = "Name;Solver;Seed;MaxWidth;Model;VarHeuristic;MergeStrategy;DistanceFunction;Time(s);Objective";
 
-        FileWriter writer = new FileWriter("tmp/setCoverElementClusterStats.csv", false);
+        String file = args[0];
+        FileWriter writer = new FileWriter("results/setCoverElementClusterStats.csv", true);
 
-        for (String file: files) {
-            SetCoverProblem problem = readInstance(file);
-            final SetCoverRanking ranking = new SetCoverRanking();
-            SetCoverRelax relax;
-            FixedWidth<SetCoverState> width;
-            final Frontier<SetCoverState> frontier = new SimpleFrontier<>(ranking);
-            final StateDistance<SetCoverState> distance = new SetCoverDistance();
-            final VariableHeuristic<SetCoverState> varh = new SetCoverHeuristics.MinCentrality(problem);
+        SetCoverProblem problem = readInstance(file);
+        final SetCoverRanking ranking = new SetCoverRanking();
+        SetCoverRelax relax;
 
-            StringBuilder csvString;
-            for (int maxWidth = 1; maxWidth < 10000; maxWidth = maxWidth + Math.max(1, (int) (maxWidth * 0.1))) {
-                boolean isExact = false;
-                for (int seed: List.of(54646)){ //, 8797, 132343)) {
-                    csvString = new StringBuilder();
-                    System.out.print(maxWidth + ", ");
-                    relax = new SetCoverRelax();
-                    width = new FixedWidth<>(maxWidth);
-                    RelaxationSolver<SetCoverState> solver = new RelaxationSolver<>(
-                            RelaxationType.GHP,
-                            problem,
-                            relax,
-                            varh,
-                            ranking,
-                            distance,
-                            null,
+        FixedWidth<SetCoverState> width;
+        final Frontier<SetCoverState> frontier = new SimpleFrontier<>(ranking);
+        final StateDistance<SetCoverState> distance = new SetCoverDistance();
+        final VariableHeuristic<SetCoverState> varh = new SetCoverHeuristics.MinCentrality(problem);
+
+        StringBuilder csvString;
+        for (int maxWidth = 1; maxWidth < 10000; maxWidth = maxWidth + Math.max(1, (int) (maxWidth * 0.1))) {
+            boolean isExact = false;
+            for (int seed: List.of(54646)){ //, 8797, 132343)) {
+                csvString = new StringBuilder();
+                System.out.print(maxWidth + ", ");
+                relax = new SetCoverRelax();
+                width = new FixedWidth<>(maxWidth);
+                RelaxationSolver<SetCoverState> solver = new RelaxationSolver<>(
+                        RelaxationType.GHP,
+                        problem,
+                        relax,
+                        varh,
+                        ranking,
+                        distance,
+                        null,
                             width,
                             frontier,
                             seed);
 
-                    long start = System.currentTimeMillis();
-                    SearchStatistics stats = solver.maximize();
-                    double duration = (System.currentTimeMillis() - start) / 1000.0;
-                    System.out.println(duration);
+                long start = System.currentTimeMillis();
+                SearchStatistics stats = solver.maximize();
+                double duration = (System.currentTimeMillis() - start) / 1000.0;
+                System.out.println(duration);
 
-                    csvString.append(file).append(";");
-                    csvString.append("relax").append(";");
-                    csvString.append(seed).append(";");
-                    csvString.append(maxWidth).append(";");
-                    csvString.append("element").append(";");
-                    csvString.append("minCentrality").append(";");
-                    csvString.append("GHPRandom").append(";");
-                    csvString.append("symDif").append(";");
-                    csvString.append(duration).append(";");
-                    csvString.append(solver.bestValue().get()).append("\n");
+                csvString.append(file).append(";");
+                csvString.append("relax").append(";");
+                csvString.append(seed).append(";");
+                csvString.append(maxWidth).append(";");
+                csvString.append("element").append(";");
+                csvString.append("minCentrality").append(";");
+                csvString.append("GHPAvgDist").append(";");
+                csvString.append("symDif").append(";");
+                csvString.append(duration).append(";");
+                csvString.append(solver.bestValue().get()).append("\n");
 
-                    writer.write(csvString.toString());
+                writer.write(csvString.toString());
 
-                    if (solver.isExact()) {
-                        isExact = true;
-                        break;
-                    }
-                }
-                if (isExact) break;
             }
         }
         writer.close();
