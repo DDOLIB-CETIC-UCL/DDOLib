6133b3e8d95101ba7578bfa5b0ed28ed297cdce2
qme <quentin.meurisse@cetic.be>
2025-07-01 08:23:58 +0200

    manage merge conflict

--- src/main/java/org/ddolib/ddo/examples/tsp/TSPMain.java
+++ src/main/java/org/ddolib/ddo/examples/tsp/TSPMain.java
@@ -8,7 +8,6 @@ import org.ddolib.ddo.implem.heuristics.FixedWidth;
 import java.io.IOException;
 import java.util.Arrays;
 
-import static org.ddolib.ddo.implem.solver.Solvers.parallelSolver;
 import static org.ddolib.ddo.implem.solver.Solvers.sequentialSolver;
 
 public class TSPMain {
@@ -25,12 +24,12 @@ public class TSPMain {
         System.out.printf("Objective: %.1f%n", solver.bestValue().get());
         System.out.println("eval from scratch: " + problem.eval(solution));
         System.out.printf("Solution : %s%n", Arrays.toString(solution));
-        if(instance.objective >= 0) {
+        if (instance.objective >= 0) {
             System.out.println("real best: " + instance.objective);
         }
     }
 
-    public static int[] extractSolution(TSPProblem problem, Solver solver){
+    public static int[] extractSolution(TSPProblem problem, Solver solver) {
         return solver.bestSolution()
                 .map(decisions -> {
                     int[] route = new int[problem.nbVars() + 1];
@@ -43,14 +42,14 @@ public class TSPMain {
                 .get();
     }
 
-    public static Solver solveTSP(TSPInstance instance){
+    public static Solver solveTSP(TSPInstance instance) {
         final TSPProblem problem = new TSPProblem(instance.distanceMatrix);
         final TSPRelax relax = new TSPRelax(problem);
         final TSPRanking ranking = new TSPRanking();
         final FixedWidth<TSPState> width = new FixedWidth<>(500);
         final DefaultVariableHeuristic<TSPState> varh = new DefaultVariableHeuristic<>();
 
-        final Frontier<TSPState> frontier = new SimpleFrontier<>(ranking,  CutSetType.LastExactLayer);
+        final Frontier<TSPState> frontier = new SimpleFrontier<>(ranking, CutSetType.LastExactLayer);
         final Solver solver = sequentialSolver(
 //                Runtime.getRuntime().availableProcessors() / 2,
                 problem,
@@ -60,7 +59,7 @@ public class TSPMain {
                 width,
                 frontier);
 
-        SearchStatistics stats = solver.maximize(2);
+        SearchStatistics stats = solver.maximize(2, false);
         System.out.println(stats);
 
         return solver;
