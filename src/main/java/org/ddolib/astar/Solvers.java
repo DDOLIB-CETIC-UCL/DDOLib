1d74e735af4305be56a5b06f7692aa19103ff983
pschaus <pschaus@gmail.com>
2025-07-04 11:41:26 +0200

    make astar work, first version

---
+++ src/main/java/org/ddolib/astar/Solvers.java
@@ -0,0 +1,21 @@
+package org.ddolib.astar;
+
+import org.ddolib.ddo.core.Frontier;
+import org.ddolib.ddo.core.Problem;
+import org.ddolib.ddo.core.Relaxation;
+import org.ddolib.ddo.heuristics.StateRanking;
+import org.ddolib.ddo.heuristics.VariableHeuristic;
+import org.ddolib.ddo.heuristics.WidthHeuristic;
+import org.ddolib.ddo.implem.dominance.DominanceChecker;
+import org.ddolib.ddo.implem.solver.SequentialSolver;
+
+public class Solvers {
+
+    public static <T, K> AStarSolver<T, K> astarSolver(Problem<T> problem,
+                                                       Relaxation<T> relax,
+                                                       VariableHeuristic<T> varh,
+                                                       StateRanking<T> ranking,
+                                                       DominanceChecker<T, K> dominance) {
+        return new AStarSolver(problem, relax, varh, ranking, dominance);
+    }
+}
---
+++ src/main/java/org/ddolib/astar/examples/knapsack/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the implementation of the knapsack problem
+ */
+package org.ddolib.astar.examples.knapsack;
