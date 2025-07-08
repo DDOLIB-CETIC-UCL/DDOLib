dd99d0b8b16b544c0064e6e4399eeb913ed47a99
BurlatsAlice <52482818+BurlatsAuguste@users.noreply.github.com>
2025-07-08 16:56:03 +0200

    add SetCoverMeasure class for xp

---
+++ src/main/java/org/ddolib/ddo/examples/setcover/elementlayer/SetCoverMeasure.java
@@ -0,0 +1,90 @@
+package org.ddolib.ddo.examples.setcover.elementlayer;
+
+import org.ddolib.ddo.core.*;
+import org.ddolib.ddo.examples.setcover.elementlayer.*;
+import org.ddolib.ddo.heuristics.StateCoordinates;
+import org.ddolib.ddo.heuristics.StateDistance;
+import org.ddolib.ddo.heuristics.VariableHeuristic;
+import org.ddolib.ddo.implem.dominance.DefaultDominanceChecker;
+import org.ddolib.ddo.implem.dominance.SimpleDominanceChecker;
+import org.ddolib.ddo.implem.frontier.SimpleFrontier;
+import org.ddolib.ddo.implem.heuristics.DefaultStateCoordinates;
+import org.ddolib.ddo.implem.heuristics.DefaultVariableHeuristic;
+import org.ddolib.ddo.implem.heuristics.FixedWidth;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.ddolib.ddo.examples.setcover.elementlayer.SetCover.readInstance;
+import static org.ddolib.ddo.implem.solver.Solvers.relaxationSolver;
+import static org.ddolib.ddo.implem.solver.Solvers.sequentialSolver;
+
+public class SetCoverMeasure {
+
+    public static void main(String[] args) throws IOException {
+        final String instance = args[0];
+        final String output = args[1];
+
+        Map<RelaxationStrat, String> stratNameMap = new HashMap<>();
+        stratNameMap.put(RelaxationStrat.Cost, "Cost");
+        stratNameMap.put(RelaxationStrat.GHP, "GHP");
+        // stratNameMap.put(RelaxationStrat.Kmeans, "Kmeans");
+
+        final SetCoverProblem problem = readInstance(instance);
+        final SetCoverRelax relax = new SetCoverRelax();
+        final SetCoverRanking ranking = new SetCoverRanking();
+        FixedWidth<SetCoverState> width;
+        VariableHeuristic<SetCoverState> varh;
+        final Frontier<SetCoverState> frontier = new SimpleFrontier<>(ranking, CutSetType.LastExactLayer);
+        final StateDistance<SetCoverState> distance = new SetCoverDistance();
+        final StateCoordinates<SetCoverState> coordinates = new DefaultStateCoordinates<>();
+
+        FileWriter writer = new FileWriter(output);
+
+        StringBuilder csvString;
+        for (RelaxationStrat relaxStrat : stratNameMap.keySet()) {
+            for (int maxWidth = 2; maxWidth < 500; maxWidth = maxWidth + Math.max(1, (int) (maxWidth * 0.1))) {
+                for (int seed: List.of(1323438797, 132343, 54646)) {
+                    varh = new SetCoverHeuristics.MinCentrality(problem);
+                    csvString = new StringBuilder();
+                    // System.out.print(maxWidth + ", ");
+                    width = new FixedWidth<>(maxWidth);
+                    Solver solver = relaxationSolver(
+                            problem,
+                            relax,
+                            varh,
+                            ranking,
+                            width,
+                            frontier,
+                            relaxStrat,
+                            distance,
+                            coordinates,
+                            seed);
+
+                    SearchStatistics stats = solver.maximize();
+                    System.out.println(stats);
+                    // System.out.println(duration);
+
+                    csvString.append(args[0]).append(";");
+                    csvString.append("sequential").append(";");
+                    csvString.append(seed).append(";");
+                    csvString.append(maxWidth).append(";");
+                    csvString.append("Knapsack").append(";");
+                    csvString.append(stratNameMap.get(relaxStrat)).append(";");
+                    csvString.append("capaDiff").append(";");
+                    csvString.append("capacity").append(";");
+                    csvString.append(stats.runTimeMS()).append(";");
+                    csvString.append(solver.bestValue().get()).append(";");
+                    csvString.append(stats.nbIterations()).append("\n");
+
+                    writer.write(csvString.toString());
+                }
+
+            }
+        }
+        writer.close();
+    }
+}
