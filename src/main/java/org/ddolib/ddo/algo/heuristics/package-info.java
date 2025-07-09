3b3a47513a5d2c63ba7139e7ec37affe9b817e1b
qme <quentin.meurisse@cetic.be>
2025-07-09 15:37:50 +0200

    complete description

--- src/main/java/org/ddolib/ddo/algo/heuristics/package-info.java
+++ src/main/java/org/ddolib/ddo/algo/heuristics/package-info.java
@@ -1,5 +1,6 @@
 /**
  * This package contains the classes and interfaces defining the heuristics
- * which can be used to customize the behavior of an MDD. It also contains
+ * which can be used to customize the behavior of an MDD. It also contains some default
+ * implementations.
  */
 package org.ddolib.ddo.algo.heuristics;
