4f08afedf22862cb7bcf27ee20dc1198a0544ccc
BurlatsAlice <52482818+BurlatsAuguste@users.noreply.github.com>
2025-07-10 16:36:03 +0200

    improve KSMeasure

--- src/main/java/org/ddolib/ddo/examples/knapsack/KSMeasure.java
+++ src/main/java/org/ddolib/ddo/examples/knapsack/KSMeasure.java
@@ -9,82 +9,107 @@ import org.ddolib.ddo.implem.frontier.SimpleFrontier;
 import org.ddolib.ddo.implem.heuristics.DefaultVariableHeuristic;
 import org.ddolib.ddo.implem.heuristics.FixedWidth;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 
 import static org.ddolib.ddo.examples.knapsack.KSMain.readInstance;
+import static org.ddolib.ddo.implem.solver.Solvers.relaxationSolver;
 import static org.ddolib.ddo.implem.solver.Solvers.sequentialSolver;
 
 public class KSMeasure {
 
     public static void main(String[] args) throws IOException {
-        final String instance = args[0];
-        final String output = args[1];
+
+        File dir = new File("./data/Knapsack/Nafar_2024");
+        FilenameFilter filter = (dir1, name) -> name.endsWith(".txt");
+        File[] instances = dir.listFiles(filter);
+
+        String header = "Name;Solver;Seed;MaxWidth;Model;VarHeuristic;MergeStrategy;DistanceFunction;CoordFunction;" +
+                "Time(ms);Objective;Optimal;nbIterations;RootFUB;Dominance;FastUpperBound\n";
+        FileWriter writer = new FileWriter("tmp/knapsackStats.csv", false);
+        writer.write(header);
+
+        // final String instance = args[0];
+        // final String output = args[1];
 
         Map<RelaxationStrat, String> stratNameMap = new HashMap<>();
         stratNameMap.put(RelaxationStrat.Cost, "Cost");
         stratNameMap.put(RelaxationStrat.GHP, "GHP");
         stratNameMap.put(RelaxationStrat.Kmeans, "Kmeans");
 
-        final KSProblem problem = readInstance(instance);
-        final KSRelax relax = new KSRelax(problem);
-        final KSRanking ranking = new KSRanking();
-        FixedWidth<Integer> width;
-        VariableHeuristic<Integer> varh;
-        SimpleDominanceChecker<Integer, Integer> dominance;
-        final Frontier<Integer> frontier = new SimpleFrontier<>(ranking, CutSetType.LastExactLayer);
-        final StateDistance<Integer> distance = new KSDistance();
-        final StateCoordinates<Integer> coordinates = new KSCoordinates();
-        final int[] seeds = {546645, 684565, 68464};
 
-        FileWriter writer = new FileWriter(output);
+        for (File file: instances) {
+            System.out.println(file.getName());
+            final KSProblem problem = readInstance(file.getPath());
+            final KSRelax relax = new KSRelax(problem);
+            final KSRanking ranking = new KSRanking();
+            FixedWidth<Integer> width;
+            VariableHeuristic<Integer> varh;
+            SimpleDominanceChecker<Integer, Integer> dominance;
+            final Frontier<Integer> frontier = new SimpleFrontier<>(ranking, CutSetType.LastExactLayer);
+            final StateDistance<Integer> distance = new KSDistance();
+            final StateCoordinates<Integer> coordinates = new KSCoordinates();
+            final int[] seeds = {546645}; //, 684565, 68464};
 
-        StringBuilder csvString;
-        for (RelaxationStrat relaxStrat : stratNameMap.keySet()) {
-            for (int maxWidth = 2; maxWidth < 500; maxWidth = maxWidth + Math.max(1, (int) (maxWidth * 0.1))) {
-                for (int seed: List.of(1323438797, 132343, 54646)) {
-                    dominance = new SimpleDominanceChecker<>(new KSDominance(), problem.nbVars());
-                    varh = new DefaultVariableHeuristic<Integer>();
-                    csvString = new StringBuilder();
-                    // System.out.print(maxWidth + ", ");
-                    width = new FixedWidth<>(maxWidth);
-                    Solver solver = sequentialSolver(
-                            problem,
-                            relax,
-                            varh,
-                            ranking,
-                            width,
-                            frontier,
-                            dominance,
-                            relaxStrat,
-                            distance,
-                            coordinates,
-                            seed);
+            StringBuilder csvString;
 
-                    SearchStatistics stats = solver.maximize();
-                    System.out.println(stats);
-                    // System.out.println(duration);
+            final HashSet<Integer> varSet = new HashSet<>();
+            for (int i = 0; i < problem.nbVars(); i++) {
+                varSet.add(i);
+            }
+            double fub = relax.fastUpperBound(problem.initialState(), varSet);
 
-                    csvString.append(args[0]).append(";");
-                    csvString.append("sequential").append(";");
-                    csvString.append(seed).append(";");
-                    csvString.append(maxWidth).append(";");
-                    csvString.append("Knapsack").append(";");
-                    csvString.append(stratNameMap.get(relaxStrat)).append(";");
-                    csvString.append("capaDiff").append(";");
-                    csvString.append("capacity").append(";");
-                    csvString.append(stats.runTimeMS()).append(";");
-                    csvString.append(solver.bestValue().get()).append(";");
-                    csvString.append(problem.optimal).append(";");
-                    csvString.append(stats.nbIterations()).append("\n");
+            for (RelaxationStrat relaxStrat : stratNameMap.keySet()) {
+                for (int maxWidth = 2; maxWidth < 500; maxWidth = maxWidth + Math.max(1, (int) (maxWidth * 0.1))) {
+                    for (int seed : seeds) {
+                        dominance = new SimpleDominanceChecker<>(new KSDominance(), problem.nbVars());
+                        varh = new DefaultVariableHeuristic<Integer>();
+                        csvString = new StringBuilder();
+                        // System.out.print(maxWidth + ", ");
+                        width = new FixedWidth<>(maxWidth);
+                        Solver solver = relaxationSolver(
+                                problem,
+                                relax,
+                                varh,
+                                ranking,
+                                width,
+                                frontier,
+                                dominance,
+                                relaxStrat,
+                                distance,
+                                coordinates,
+                                seed);
 
-                    writer.write(csvString.toString());
+                        SearchStatistics stats = solver.maximize();
+                        // System.out.println(stats);
+                        // System.out.println(duration);
+
+                        csvString.append(file.getName()).append(";"); // Name
+                        csvString.append("relaxation").append(";"); // solver
+                        csvString.append(seed).append(";"); // Seed
+                        csvString.append(maxWidth).append(";"); // maxWidth
+                        csvString.append("Knapsack").append(";"); // problem
+                        csvString.append(stratNameMap.get(relaxStrat)).append(";"); // relaxStrat
+                        csvString.append("capaDiff").append(";"); // distance function
+                        csvString.append("capacity").append(";"); // coordinate function
+                        csvString.append(stats.runTimeMS()).append(";"); // runtime
+                        csvString.append(solver.bestValue().get()).append(";"); // Objective
+                        csvString.append(problem.optimal).append(";"); // optimal value
+                        csvString.append(stats.nbIterations()).append(";"); // nb iterations
+                        csvString.append(fub).append(";"); // value of fast upper bound
+                        csvString.append("true").append(";"); // Use dominance ?
+                        csvString.append("true").append("\n"); // Use fast upper bound ?
+
+                        writer.write(csvString.toString());
+                    }
                 }
-
             }
         }
         writer.close();
