cffd1f2daeb6a06c469dffd1ecc27921b623ab05
BastienAubecq <bastien.aubecq@student.uclouvain.be>
2025-07-30 13:59:25 +0200

    Ordered variable heuristic

--- src/main/java/org/ddolib/examples/ddo/carseq/CSMain.java
+++ src/main/java/org/ddolib/examples/ddo/carseq/CSMain.java
@@ -7,6 +7,7 @@ import org.ddolib.ddo.core.frontier.CutSetType;
 import org.ddolib.ddo.core.frontier.Frontier;
 import org.ddolib.ddo.core.frontier.SimpleFrontier;
 import org.ddolib.ddo.core.heuristics.variable.DefaultVariableHeuristic;
+import org.ddolib.ddo.core.heuristics.variable.OrderedVariableHeuristic;
 import org.ddolib.ddo.core.heuristics.variable.VariableHeuristic;
 import org.ddolib.ddo.core.heuristics.width.FixedWidth;
 import org.ddolib.ddo.core.profiling.SearchStatistics;
@@ -34,7 +35,7 @@ public class CSMain {
         CSFastUpperBound fub = new CSFastUpperBound(problem);
         CSRanking ranking = new CSRanking();
         FixedWidth<CSState> width = new FixedWidth<>(500);
-        VariableHeuristic<CSState> varh = new CSVariableHeuristic(problem);
+        VariableHeuristic<CSState> varh = new OrderedVariableHeuristic<>(problem);
         Frontier<CSState> frontier = new SimpleFrontier<>(ranking, CutSetType.LastExactLayer);
         SimpleDominanceChecker<CSState, Integer> dominance = new SimpleDominanceChecker<>(new CSDominance(problem), problem.nbVars());
         Solver solver = Solvers.sequentialSolver(
--- src/test/java/org/ddolib/examples/ddo/carseq/CSTest.java
+++ src/test/java/org/ddolib/examples/ddo/carseq/CSTest.java
@@ -7,9 +7,11 @@ import org.ddolib.ddo.core.frontier.CutSetType;
 import org.ddolib.ddo.core.frontier.Frontier;
 import org.ddolib.ddo.core.frontier.SimpleFrontier;
 import org.ddolib.ddo.core.heuristics.variable.DefaultVariableHeuristic;
+import org.ddolib.ddo.core.heuristics.variable.OrderedVariableHeuristic;
 import org.ddolib.ddo.core.heuristics.variable.VariableHeuristic;
 import org.ddolib.ddo.core.heuristics.width.FixedWidth;
 import org.ddolib.factory.Solvers;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -35,11 +37,6 @@ public class CSTest {
     }
 
     @Test
-    public void testSolveBig() throws IOException {
-        testSolve("data/CarSeq/big.txt");
-    }
-
-    @Test
     public void testSolveInstances() throws IOException {
         File dir = new File("data/CarSeq");
         for (File file : dir.listFiles()) {
@@ -59,7 +56,7 @@ public class CSTest {
         CSFastUpperBound fub = new CSFastUpperBound(problem);
         CSRanking ranking = new CSRanking();
         FixedWidth<CSState> width = new FixedWidth<>(500);
-        VariableHeuristic<CSState> varh = new CSVariableHeuristic(problem);
+        VariableHeuristic<CSState> varh = new OrderedVariableHeuristic<>(problem);
         Frontier<CSState> frontier = new SimpleFrontier<>(ranking, CutSetType.LastExactLayer);
         SimpleDominanceChecker<CSState, Integer> dominance = new SimpleDominanceChecker<>(new CSDominance(problem), problem.nbVars());
         Solver solver = Solvers.sequentialSolver(
