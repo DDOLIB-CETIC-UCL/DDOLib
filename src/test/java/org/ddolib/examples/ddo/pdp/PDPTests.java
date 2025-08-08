a49f267a7e49b69f9f1bf28d658389dc7de98074
qme <quentin.meurisse@cetic.be>
2025-08-08 08:00:35 +0200

    remove deprecated tests

--- src/test/java/org/ddolib/examples/ddo/pdp/PDPTests.java
+++ src/test/java/org/ddolib/examples/ddo/pdp/PDPTests.java
@@ -1,7 +1,6 @@
 package org.ddolib.examples.ddo.pdp;
 
 import org.ddolib.common.dominance.DefaultDominanceChecker;
-import org.ddolib.common.solver.Solver;
 import org.ddolib.ddo.core.frontier.CutSetType;
 import org.ddolib.ddo.core.frontier.Frontier;
 import org.ddolib.ddo.core.frontier.SimpleFrontier;
@@ -12,18 +11,12 @@ import org.ddolib.util.testbench.SolverConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PDPTests {
 
@@ -74,22 +67,5 @@ public class PDPTests {
     public Stream<DynamicTest> testPDP() {
         var bench = new PDPBench();
         return bench.generateTests();
-    }
-
-    static Stream<PDPInstance> dataProvider2() {
-        return IntStream.range(0, 10).boxed().map(i ->
-                PDPMain.genInstance(5 + i % 14, i % 3, new Random(i)));
-    }
-
-    @ParameterizedTest
-    @MethodSource("dataProvider2")
-    public void testPDP(PDPInstance instance) {
-
-        PDPProblem problem = new PDPProblem(instance);
-        Solver s = PDPMain.solveDPD(problem);
-
-        PDPSolution solution = PDPMain.extractSolution(s, problem);
-
-        assertEquals(solution.value, instance.eval(solution.solution));
     }
 }
