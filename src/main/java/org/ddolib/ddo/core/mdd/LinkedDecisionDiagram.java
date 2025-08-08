67df49cdd44966d823c3168bf7708de9d4f8eb5a
qme <quentin.meurisse@cetic.be>
2025-08-08 11:43:28 +0200

    add fub checker for exact nodes

--- src/main/java/org/ddolib/ddo/core/mdd/LinkedDecisionDiagram.java
+++ src/main/java/org/ddolib/ddo/core/mdd/LinkedDecisionDiagram.java
@@ -11,6 +11,10 @@ import org.ddolib.modeling.Problem;
 import org.ddolib.modeling.Relaxation;
 import org.ddolib.modeling.StateRanking;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.Map.Entry;
@@ -99,6 +103,8 @@ public final class LinkedDecisionDiagram<T, K> implements DecisionDiagram<T, K> 
          */
         private boolean isMarked;
 
+        private double ub = Double.POSITIVE_INFINITY;
+
         /**
          * Creates a new node
          */
@@ -118,6 +124,10 @@ public final class LinkedDecisionDiagram<T, K> implements DecisionDiagram<T, K> 
          */
         public void setNodeType(final NodeType nodeType) {
             this.type = nodeType;
+        }
+
+        public void setUb(final double ub) {
+            this.ub = ub;
         }
 
         /**
@@ -299,6 +309,7 @@ public final class LinkedDecisionDiagram<T, K> implements DecisionDiagram<T, K> 
                     continue;
                 } else {
                     double rub = saturatedAdd(node.value, input.fub().fastUpperBound(state, variables));
+                    node.setUb(rub);
                     this.currentLayer.add(new NodeSubProblem<>(state, rub, node));
                 }
             }
@@ -409,6 +420,9 @@ public final class LinkedDecisionDiagram<T, K> implements DecisionDiagram<T, K> 
         if (input.compilationType() == CompilationType.Relaxed) {
             computeLocalBounds();
         }
+        if (input.compilationType() != CompilationType.Relaxed) {
+            checkFub();
+        }
     }
 
     @Override
@@ -471,6 +485,38 @@ public final class LinkedDecisionDiagram<T, K> implements DecisionDiagram<T, K> 
         }
         dotStr.append("}");
         return dotStr.toString();
+    }
+
+    private void checkFub() {
+        DecimalFormat df = new DecimalFormat("#.##########");
+        for (Node last : nextLayer.values()) {
+            double lastValue = last.value;
+            //For each node we save the longest path to last
+            LinkedHashMap<Node, Double> parent = new LinkedHashMap<>();
+            parent.put(last, 0.0);
+            while (!parent.isEmpty()) {
+                Entry<Node, Double> current = parent.pollFirstEntry();
+                double longest = current.getKey().value + current.getValue();
+                if (current.getKey().ub < longest) {
+                    String dot = exportAsDot();
+                    try (BufferedWriter bw =
+                                 new BufferedWriter(new FileWriter(Paths.get("output",
+                                         "failed.dot").toString()))) {
+                        bw.write(dot);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    String failureMsg = String.format("Found node with upper bound (%s) lower than " +
+                            "its longest path (%s)", df.format(current.getKey().ub), df.format(lastValue));
+                    throw new RuntimeException(failureMsg);
+                }
+
+                for (Edge edge : current.getKey().edges) {
+                    double longestFromParent = parent.getOrDefault(edge.origin, Double.NEGATIVE_INFINITY);
+                    parent.put(edge.origin, Double.max(longestFromParent, edge.weight + current.getValue()));
+                }
+            }
+        }
     }
 
     // --- UTILITY METHODS -----------------------------------------------
