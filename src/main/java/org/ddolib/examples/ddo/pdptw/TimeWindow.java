194f97f5ae8b0c1559731836a1b8e9e747286bf3
rdl <renaud.delandtsheer@cetic.be>
2025-08-25 15:15:32 +0200

    polishing

--- src/main/java/org/ddolib/examples/ddo/pdptw/TimeWindow.java
+++ src/main/java/org/ddolib/examples/ddo/pdptw/TimeWindow.java
@@ -1,4 +1,7 @@
 package org.ddolib.examples.ddo.pdptw;
 
 public record TimeWindow(int start, int end) {
+    public int entryTime(int arrivalTime){
+        return Math.max(arrivalTime, start);
+    }
 }
