8f126562e42fe2c54f23c0ca5e5f3ff662db3b3f
Quentin Meurisse <quentin.meurisse@cetic.be>
2025-09-03 13:06:27 +0200

    20 implement max2sat example (#47)
    * first class to model Max2Sat
    
    * transitions functions
    
    * some correction + first test
    
    * renaming + remove useless parameter from state
    
    * replace the state with an ArrayList
    
    * improve hashcode
    
    * change the example
    
    * add description for the ranking
    
    * add relaxation
    
    * naive solver
    
    * IO for Max2Sat
    
    * correct bug with unordered variables
    
    * order the clause in output files
    
    * add description
    
    * add optimal field in problem
    
    * unit tests
    
    * first attempt rub
    
    * rub v2
    
    * make Max2SatState to have the depth of each state
    
    * precompute some part of the rub
    
    * try more examples
    
    * add comments
    
    * manage unary clauses
    
    * tests with unary clauses
    
    * remove print
    
    * remove other print
    
    * improve file reading
    
    * make Max2Sat example runnable from the example
    
    * add unit test with relaxation
    
    * add toString for tests
    
    * use Paths to manage os specific files separators
    
    * Update src/main/java/org/ddolib/ddo/examples/max2sat/NaiveMax2SatSolver.java
    
    * Apply suggestions from code review
    
    Co-authored-by: FabianGermeau <142988244+FabianGermeau@users.noreply.github.com>
    
    * change parameters name
    
    * update description
    
    * manage merge conflicts
    
    * add package info
    
    * print used seed when generating random instance
    
    * use getOrElse
    
    * correct fub
    
    * update the solver config + use test bench
    
    * Apply suggestions from code review
    
    Co-authored-by: FabianGermeau <142988244+FabianGermeau@users.noreply.github.com>
    
    * correct javadoc
    
    * add description for the initial value
    
    * add comment in relaxation
    
    * Update src/main/java/org/ddolib/examples/ddo/max2sat/Max2SatProblem.java
    
    Co-authored-by: FabianGermeau <142988244+FabianGermeau@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: FabianGermeau <142988244+FabianGermeau@users.noreply.github.com>

---
+++ src/main/java/org/ddolib/examples/ddo/max2sat/BinaryClause.java
@@ -0,0 +1,90 @@
+package org.ddolib.examples.ddo.max2sat;
+
+import java.util.Objects;
+
+import static java.lang.Math.abs;
+
+/**
+ * Class to model a Binary clause of two literals for CNF formula. <br>
+ * <p>
+ * To symbolize a literal {@code x_i    }, for {@code i > 0}, we give the value {@code i} as
+ * input. To symbolize {@code NOT x_i}, we give {@code -i}.
+ */
+public class BinaryClause implements Comparable<BinaryClause> {
+    public final int i;
+    public final int j;
+
+    public BinaryClause(int i, int j) {
+        if (i == 0 || j == 0)
+            throw new IllegalArgumentException("Id of variable in Binary clauses must be != 0");
+        this.i = i;
+        this.j = j;
+    }
+
+    /**
+     * Evaluates if the clause is verified given 2 boolean values.
+     *
+     * @param a The value to attribute to the variable {@code x_i} (0 or 1).
+     * @param b The value to attribute to the variable {@code x_j} (0 or 1).
+     * @return 0 if the clause is verified. 1 else.
+     */
+    public int eval(int a, int b) {
+        int literal1 = i < 0 ? a ^ 1 : a;
+        int literal2 = j < 0 ? b ^ 1 : b;
+        return literal1 | literal2;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof BinaryClause other) return this.i == other.i && this.j == other.j;
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(i, j);
+    }
+
+    @Override
+    public String toString() {
+        String notX = i < 0 ? "!" : "";
+        String notY = j < 0 ? "!" : "";
+        return String.format("%sx_%d || %sx_%d", notX, abs(i), notY, abs(j));
+    }
+
+    private int internalCompare(int x, int y) {
+        int compared = Integer.compare(abs(x), abs(y));
+        if (compared == 0) {
+            return -Integer.compare(x, y);
+        } else {
+            return compared;
+        }
+    }
+
+    /**
+     * Used to compare binary clauses. It is used to sort them when generating Max2Sat instances.
+     * <br>
+     * The induced order is the following:
+     * <ol>
+     *     <li>The lexical order on the literals' indices. </li>
+     *     <li>The positive literal before the negative ones.</li>
+     * </ol>
+     *
+     * @param other The binary clause to be compared.
+     * @return <ul>
+     *     <li> {@code 0} if if {@code this == other}</li>
+     *     <li><code>1</code> if {@code this > other}</li>
+     *     <li><code>-1</code> if {@code this < other}</li>
+     * </ul>
+     */
+    @Override
+    public int compareTo(BinaryClause other) {
+        int lit1Compare = internalCompare(this.i, other.i);
+        if (lit1Compare == 0) {
+            return internalCompare(this.j, other.j);
+        } else {
+            return lit1Compare;
+        }
+    }
+}
