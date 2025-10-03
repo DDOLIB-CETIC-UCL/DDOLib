cfb29e9bf5f18f24325dffd9fb21fdebb0248b02
qme <quentin.meurisse@cetic.be>
2025-10-03 11:07:35 +0200

    add javadoc

--- src/main/java/org/ddolib/common/dominance/DominanceChecker.java
+++ src/main/java/org/ddolib/common/dominance/DominanceChecker.java
@@ -10,8 +10,16 @@ import org.ddolib.modeling.Dominance;
  */
 public abstract class DominanceChecker<T, K> {
 
+    /**
+     * Problem specific dominance rule.
+     */
     protected final Dominance<T, K> dominance;
 
+    /**
+     * Instantiate a new Dominance checker.
+     *
+     * @param dominance The problem specific dominance rule used by the checker.
+     */
     protected DominanceChecker(Dominance<T, K> dominance) {
         this.dominance = dominance;
     }
@@ -25,7 +33,20 @@ public abstract class DominanceChecker<T, K> {
      */
     public abstract void updateDominance(T state, int depth, double objValue);
 
+    /**
+     * Returns whether the input state is dominated
+     *
+     * @param state    The state on which test dominance.
+     * @param depth    The depth of the state in the MDD.
+     * @param objValue The length of the longest path from the root to the input state.
+     * @return Whether {@code state} is dominated by another state at the same depth.
+     */
     public abstract boolean isDominated(T state, int depth, double objValue);
 
+    /**
+     * Returns a new checker, clearing the internal states.
+     *
+     * @return An all new checker
+     */
     public abstract DominanceChecker<T, K> clear();
 }
