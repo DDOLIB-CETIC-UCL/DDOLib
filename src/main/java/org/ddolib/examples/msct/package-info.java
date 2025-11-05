b59b94dcbd072255a912179390f39e2f0f521cb2
qme <quentin.meurisse@cetic.be>
2025-11-05 13:03:25 +0100

    add links to cited paper in package-info

--- src/main/java/org/ddolib/examples/msct/package-info.java
+++ src/main/java/org/ddolib/examples/msct/package-info.java
@@ -4,8 +4,9 @@
  * - each job is scheduled after its release time
  * - the sum of completion time is minimized
  * This DP model is from :
- * 	J. Christopher Beck, Ryo Kuroiwa, Jimmy H. M. Lee, Peter J. Stuckey, Allen Z. Zhong:
- * Transition Dominance in Domain-Independent Dynamic Programming. CP 2025: 5:1-5:23
+ * <a href="https://drops.dagstuhl.de/storage/00lipics/lipics-vol340-cp2025/LIPIcs.CP.2025.5/LIPIcs.CP.2025.5.pdf">
+ * J. Christopher Beck, Ryo Kuroiwa, Jimmy H. M. Lee, Peter J. Stuckey, Allen Z. Zhong:
+ * Transition Dominance in Domain-Independent Dynamic Programming. CP 2025: 5:1-5:23</a>
  * In this model a state is represented by:
  * - the set of remaining jobs
  * - the current time (the end time of last sequenced job)
--- src/main/java/org/ddolib/examples/smic/package-info.java
+++ src/main/java/org/ddolib/examples/smic/package-info.java
@@ -5,7 +5,11 @@
  * processing time p ∈ R+, a release date r ∈ R+ and a positive (resp. negative) inventory
  * modification for loading (resp. unloading) task. The objective is to sequence
  * the jobs in J such that the makespan is minimized while the inventory is between a given range.
- * This problem is considered in the paper: Morteza Davari, Mohammad Ranjbar, Patrick De Causmaecker, Roel Leus:
- * Minimizing makespan on a single machine with release dates and inventory constraints. Eur. J. Oper. Res. 286(1): 115-128 (2020)
+ * This problem is considered in the paper:
+ * <p>
+ * <a href="https://www.sciencedirect.com/science/article/abs/pii/S0377221720302435"> Morteza Davari, Mohammad Ranjbar, Patrick De
+ * Causmaecker, Roel Leus:
+ * Minimizing makespan on a single machine with release dates and inventory constraints. Eur. J.
+ * Oper. Res. 286(1): 115-128 (2020)</a>
  */
 package org.ddolib.examples.smic;
