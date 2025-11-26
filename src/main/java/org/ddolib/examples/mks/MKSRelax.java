0df412900f1e30a78e2394cdb0bbdce22b9a191a
AliceBurlats <alice.burlats@gmail.com>
2025-11-26 09:19:30 +0100

    add MKS model

---
+++ src/main/java/org/ddolib/examples/mks/MKSRelax.java
@@ -0,0 +1,30 @@
+package org.ddolib.examples.mks;
+
+import org.ddolib.ddo.core.Decision;
+import org.ddolib.modeling.Relaxation;
+
+import java.util.Iterator;
+
+import static java.lang.Math.max;
+
+public class MKSRelax implements Relaxation<MKSState> {
+
+    @Override
+    public MKSState mergeStates(Iterator<MKSState> states) {
+        assert states.hasNext();
+        MKSState state = states.next();
+        double[] capa = state.capacities.clone();
+        while (states.hasNext()) {
+            state = states.next();
+            for (int dim = 0; dim < capa.length; dim++) {
+                capa[dim] = max(capa[dim], state.capacities[dim]);
+            }
+        }
+        return new MKSState(capa);
+    }
+
+    @Override
+    public double relaxEdge(MKSState from, MKSState to, MKSState merged, Decision d, double cost) {
+        return cost;
+    }
+}
