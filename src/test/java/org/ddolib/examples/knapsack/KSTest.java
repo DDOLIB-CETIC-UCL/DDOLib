0ed0af006973c3deb0131e14d223094b63058c93
qme <quentin.meurisse@cetic.be>
2025-12-04 10:27:45 +0100

    update file reading for KS

--- src/test/java/org/ddolib/examples/knapsack/KSTest.java
+++ src/test/java/org/ddolib/examples/knapsack/KSTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public class KSTest {
     @DisplayName("Knapsack")
     @TestFactory
     public Stream<DynamicTest> testKS() {
-        var dataSupplier = new KSTestDataSupplier(Paths.get("src", "test", "resources", "Knapsack").toString());
+        var dataSupplier = new KSTestDataSupplier(Path.of("src", "test", "resources", "Knapsack"));
         var bench = new ProblemTestBench<>(dataSupplier);
         bench.testRelaxation = true;
         bench.testFLB = true;
