e9d8394a856b5de6cb541be8d615cf4c289b3b22
qme <quentin.meurisse@cetic.be>
2025-12-04 10:45:53 +0100

    update file reading for SRFLP

--- src/test/java/org/ddolib/examples/srflp/SRFLPTest.java
+++ src/test/java/org/ddolib/examples/srflp/SRFLPTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public class SRFLPTest {
@@ -14,7 +14,7 @@ public class SRFLPTest {
     @TestFactory
     public Stream<DynamicTest> testSRFLP() {
         var dataSupplier =
-                new SRFLPTestDataSupplier(Paths.get("src", "test", "resources", "SRFLP").toString());
+                new SRFLPTestDataSupplier(Path.of("src", "test", "resources", "SRFLP"));
         var bench = new ProblemTestBench<>(dataSupplier);
         bench.testRelaxation = true;
         bench.testFLB = true;
