7b150013120eb15409ce7cae0183b3c009044300
kameugne <rkameugne@gmail.com>
2025-12-11 15:59:54 +0100

    example paper

--- src/main/java/org/ddolib/examples/maximumcoverage/PaperExample.java
+++ src/main/java/org/ddolib/examples/maximumcoverage/PaperExample.java
@@ -15,12 +15,13 @@ import java.util.BitSet;
 
 public class PaperExample {
     public static void main(String[] args) {
-        int n = 4; int m = 4; int k = 3;
+        int n = 5; int m = 4; int k = 3;
         BitSet[] ss = new BitSet[m];
-        ss[0] = new BitSet(n);  ss[0].set(0);  ss[0].set(1);
-        ss[1] = new BitSet(n);  ss[1].set(2);  ss[1].set(3);
-        ss[2] = new BitSet(n);  /*ss[2].set(1);*/  ss[2].set(3);
-        ss[3] = new BitSet(n);  /*ss[3].set(3);*/  ss[3].set(2);
+        ss[0] = new BitSet(n);  ss[0].set(0);  ss[0].set(2);
+        ss[1] = new BitSet(n);  ss[1].set(1);  ss[1].set(3); ss[1].set(3);
+        ss[2] = new BitSet(n);  ss[2].set(3);  /*ss[2].set(1);*/
+        ss[3] = new BitSet(n);  ss[3].set(1);  ss[3].set(4);
+//        ss[4] = new BitSet(n);  ss[4].set(1);  ss[4].set(3);
         MaxCoverProblem problem = new MaxCoverProblem(n, m, k, ss);
         System.out.println(problem);
         DdoModel<MaxCoverState> model = new DdoModel<>() {
@@ -52,7 +53,7 @@ public class PaperExample {
             @Override
             public ReductionStrategy<MaxCoverState> relaxStrategy() {
                 return new GHP<>(new MaxCoverDistance(problem));
-                //return new CostBased<>(new MaxCoverRanking());
+//                return new CostBased<>(new MaxCoverRanking());
                 //return new Kmeans<>(new MaxCoverCoordinates(problem));
                 // return new Hybrid<>(new MaxCoverRanking(), new MaxCoverDistance(problem));
 
@@ -61,7 +62,7 @@ public class PaperExample {
             @Override
             public ReductionStrategy<MaxCoverState> restrictStrategy() {
                 return new GHP<>(new MaxCoverDistance(problem));
-                //return new CostBased<>(new MaxCoverRanking());
+//                return new CostBased<>(new MaxCoverRanking());
                 // return new Kmeans<>(new MaxCoverCoordinates(problem));
                 //return new Hybrid<>(new MaxCoverRanking(), new MaxCoverDistance(problem));
             }
