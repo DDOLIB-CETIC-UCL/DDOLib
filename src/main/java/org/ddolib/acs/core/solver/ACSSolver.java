417559f59f40c516e2be22c91d5cb30ff5a8fdfb
kameugne <rkameugne@gmail.com>
2026-01-21 13:54:44 +0100

    acs gap

--- src/main/java/org/ddolib/acs/core/solver/ACSSolver.java
+++ src/main/java/org/ddolib/acs/core/solver/ACSSolver.java
@@ -427,16 +427,21 @@ public final class ACSSolver<T> implements Solver {
             return Double.NaN;
         } else {
             double maxLB = Double.NEGATIVE_INFINITY;
+            double minUB = Double.POSITIVE_INFINITY;
             for (int i = 0; i < problem.nbVars(); i++) {
                 if (!open.get(i).isEmpty()) {
                     if (negativeTransitionCosts) {
-                        maxLB = Math.max(maxLB, Math.abs(open.get(i).peek().f()));
+                        minUB = Math.min(minUB, open.get(i).peek().f());
                     } else {
                         maxLB = Math.max(maxLB, open.get(i).peek().f());
                     }
                 }
             }
-            return Math.abs(100.0 * (bestUB - maxLB) / Math.abs(bestUB));
+            if (negativeTransitionCosts) {
+                return Math.abs(100.0 * (minUB - bestUB) / Math.abs(bestUB));
+            } else {
+                return Math.abs(100.0 * (bestUB - maxLB) / Math.abs(bestUB));
+            }
         }
     }
 }
