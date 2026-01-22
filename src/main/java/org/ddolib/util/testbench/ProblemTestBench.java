f97e316a067b270029563b60a71841c46f411cdd
qme <quentin.meurisse@cetic.be>
2026-01-22 15:41:57 +0100

    add test on AWA* in the test bench

--- src/main/java/org/ddolib/util/testbench/ProblemTestBench.java
+++ src/main/java/org/ddolib/util/testbench/ProblemTestBench.java
@@ -110,6 +110,7 @@ public class ProblemTestBench<T, P extends Problem<T>> {
             case DdoModel<T> ddoModel when width == -1 -> Solvers.minimizeExact(ddoModel);
             case DdoModel<T> ddoModel -> Solvers.minimizeDdo(ddoModel);
             case AcsModel<T> acsModel -> Solvers.minimizeAcs(acsModel);
+            case AwAstarModel<T> awastarModel -> Solvers.minimizeAwAStar(awastarModel);
             default -> Solvers.minimizeAstar(model);
         };
 
@@ -353,6 +354,35 @@ public class ProblemTestBench<T, P extends Problem<T>> {
         testSolverResult(testModel);
     }
 
+
+    private void testAwAstarSolver(P problem) throws InvalidSolutionException {
+        Model<T> globalModel = model.apply(problem);
+
+        AwAstarModel<T> testModel = new AwAstarModel<>() {
+            @Override
+            public Problem<T> problem() {
+                return problem;
+            }
+
+            @Override
+            public FastLowerBound<T> lowerBound() {
+                return globalModel.lowerBound();
+            }
+
+            @Override
+            public DominanceChecker<T> dominance() {
+                return globalModel.dominance();
+            }
+
+            @Override
+            public DebugLevel debugMode() {
+                return DebugLevel.ON;
+            }
+        };
+
+        testSolverResult(testModel);
+    }
+
     /**
      * Test if the mode with the relaxation and the fast lower bound enabled lead to the optimal solution. As side
      * effect, it tests if the fast lower bound on merged states does not cause errors.
@@ -478,6 +508,10 @@ public class ProblemTestBench<T, P extends Problem<T>> {
                 DynamicTest.dynamicTest(String.format("ACS for %s", p.toString()), () -> testACSSolver(p))
         );
         allTests = Stream.concat(allTests, acsTests);
+
+        Stream<DynamicTest> awastarTests = problems.stream().map(p -> DynamicTest.dynamicTest(
+                String.format("AWA* for %s", p.toString()), () -> testAwAstarSolver(p)));
+        allTests = Stream.concat(allTests, awastarTests);
 
         return allTests;
     }
