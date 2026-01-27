235756720114d1dbb88e8ae359253210421ca443
Quentin Meurisse <quentin.meurisse@cetic.be>
2026-01-27 15:12:16 +0100

    add doc to Solution (#211)

--- src/main/java/org/ddolib/common/solver/Solution.java
+++ src/main/java/org/ddolib/common/solver/Solution.java
@@ -6,13 +6,22 @@ import java.util.Arrays;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * Wrapper defining a solution from a set of decisions
+ *
+ */
 public class Solution {
-
 
     private final int[] solution;
     private final SearchStatistics statistics;
 
 
+    /**
+     * Constructs a solution given a set of decision and {@link SearchStatistics} on this set.
+     *
+     * @param decisions  the set of decision leading to this solution
+     * @param statistics the statistics related to this solution
+     */
     public Solution(Optional<Set<Decision>> decisions, SearchStatistics statistics) {
         if (decisions.isPresent()) {
             solution = new int[decisions.get().size()];
@@ -26,14 +35,31 @@ public class Solution {
         this.statistics = statistics;
     }
 
+    /**
+     * Returns the evaluation of the objective value of this solution.
+     *
+     * @return the evaluation of the objective value of this solution
+     */
     public double value() {
         return statistics.incumbent();
     }
 
+    /**
+     * Returns an array {@code t} such that {@code t[i]} is the assigned value to the decision
+     * variable {@code x_i}.
+     *
+     * @return an array {@code t} such that {@code t[i]} is the assigned value to the decision
+     * variable {@code x_i}.
+     */
     public int[] solution() {
         return solution;
     }
 
+    /**
+     * Returns the statistics associated to this solution.
+     *
+     * @return the statistics associated to this solution
+     */
     public SearchStatistics statistics() {
         return statistics;
     }
