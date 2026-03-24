a041307610db5c0b4daf070057277da5aa5685bf
qme <quentin.meurisse@cetic.be>
2026-03-24 10:21:44 +0100

    sort items in knapsack when loading instance

--- src/main/java/org/ddolib/examples/knapsack/KSAstarMain.java
+++ src/main/java/org/ddolib/examples/knapsack/KSAstarMain.java
@@ -42,7 +42,8 @@ public class KSAstarMain {
      * @throws IOException if the instance file cannot be read
      */
     public static void main(final String[] args) throws IOException {
-        final String instance = args.length == 0 ? Path.of("data", "Knapsack", "instance_n1000_c1000_10_5_10_5_0").toString() : args[0];
+        final String instance = args.length == 0 ? Path.of("data", "Knapsack",
+                "instance_n1000_c1000_10_5_10_5_0").toString() : args[0];
         final KSProblem problem = new KSProblem(instance);
         final Model<Integer> model = new Model<>() {
             @Override
