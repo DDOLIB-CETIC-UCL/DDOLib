bbff6b2249fb93a15d955f0c1f7a4afaa63dc9af
qme <quentin.meurisse@cetic.be>
2026-04-23 15:40:27 +0200

    update SearchStatistics

--- src/main/java/org/ddolib/common/solver/SearchStatistics.java
+++ src/main/java/org/ddolib/common/solver/SearchStatistics.java
@@ -1,34 +1,130 @@
 package org.ddolib.common.solver;
 
-import org.ddolib.util.PrettyPrint;
+public final class SearchStatistics {
+    private final long _t0;
+    private long _lastTimeOfImprovement;
+    private long _currentTime;
+    private SearchStatus _status = SearchStatus.UNKNOWN;
+    private int _nbIteration = 0;
+    private int _lastIterationOfImprovement = 0;
+    private double _incumbent;
+    private double _prevIncumbent = Double.POSITIVE_INFINITY;
+    private double _gap = 1.0;
+    private int _frontierMaxSize = 0;
 
-public record SearchStatistics(
-        SearchStatus status,
-        int nbIterations,
-        int queueMaxSize,
-        long runTimeMs,
-        double incumbent,
-        double gap) {
 
-    public String toCSV() {
-        return String.format("%s;%d;%d;%d;%f;%f",
-                status,
-                nbIterations,
-                queueMaxSize,
-                runTimeMs,
-                incumbent,
-                gap);
+    public SearchStatistics(long startTime, double initValue) {
+        _t0 = startTime;
+        _lastTimeOfImprovement = startTime;
+        _currentTime = startTime;
+        _incumbent = initValue;
     }
+
+    public long lastTimeOfImprovement() {
+        return _lastTimeOfImprovement;
+    }
+
+    public long runTime() {
+        return _currentTime - _t0;
+    }
+
+    public int nbIteration() {
+        return _nbIteration;
+    }
+
+    public int lastIterationOfImprovement() {
+        return _lastIterationOfImprovement;
+    }
+
+    public double gap() {
+        return _gap;
+    }
+
+    public double incumbent() {
+        return _incumbent;
+    }
+
+    public double prevIncumbent() {
+        return _prevIncumbent;
+    }
+
+    public SearchStatus status() {
+        return _status;
+    }
+
+    public int frontierMaxSize() {
+        return _frontierMaxSize;
+    }
+
+    public SearchStatistics copy() {
+        SearchStatistics clone = new SearchStatistics(this._t0, this._incumbent);
+
+        clone._currentTime = this._currentTime;
+        clone._lastTimeOfImprovement = this._lastTimeOfImprovement;
+        clone._status = this._status;
+        clone._nbIteration = this._nbIteration;
+        clone._lastIterationOfImprovement = this._lastIterationOfImprovement;
+        clone._prevIncumbent = this._prevIncumbent;
+        clone._gap = this._gap;
+        clone._frontierMaxSize = this.frontierMaxSize();
+
+        return clone;
+    }
+
+    public SearchStatistics updateIncumbent(double incumbent, double gap) {
+        SearchStatistics toReturn = this.copy();
+        toReturn._incumbent = incumbent;
+        toReturn._prevIncumbent = this._incumbent;
+        toReturn._currentTime = System.currentTimeMillis();
+        toReturn._lastTimeOfImprovement = toReturn._currentTime;
+        toReturn._lastIterationOfImprovement = this._nbIteration;
+        toReturn._gap = gap;
+
+        return toReturn;
+    }
+
+    public SearchStatistics updateStatus(SearchStatus status) {
+        SearchStatistics toReturn = this.copy();
+        toReturn._status = status;
+        return toReturn;
+    }
+
+    public SearchStatistics incrementNbIter() {
+        SearchStatistics toReturn = this.copy();
+        toReturn._nbIteration++;
+        return toReturn;
+    }
+
+    public SearchStatistics updateFrontierMaxSize(int frontierSize) {
+        SearchStatistics toReturn = this.copy();
+        toReturn._frontierMaxSize = Integer.max(this._frontierMaxSize, frontierSize);
+        return toReturn;
+    }
+
+    public SearchStatistics updateGap(double gap) {
+        SearchStatistics toReturn = this.copy();
+        toReturn._gap = gap;
+        return toReturn;
+    }
+
+    public SearchStatistics updateTime(long time) {
+        SearchStatistics toReturn = this.copy();
+        toReturn._currentTime = time;
+        return toReturn;
+    }
+
+
+
 
     @Override
     public String toString() {
-        return "\n\tstatus = " + status +
-                "\n\tnbIterations = " + nbIterations +
-                "\n\tqueueMaxSize = " + queueMaxSize +
-                "\n\trunTimeMs = " + PrettyPrint.formatMs(runTimeMs) +
-                "\n\tincumbent = " + (incumbent == Double.POSITIVE_INFINITY || incumbent == Double.NEGATIVE_INFINITY ? "+-∞" : incumbent) +
-                "\n\tgap = " + (gap == Double.POSITIVE_INFINITY ? "∞" : gap) +
-                "\n";
+        String str = "\n\tstatus: " + status();
+        str += "\n\tnbIterations: " + nbIteration();
+        str += "\n\tfrontierMaxSize: " + frontierMaxSize();
+        str += "\n\truntime: " + runTime();
+        str += "\n\tincumbent: " + (Double.isInfinite(incumbent()) ? "+-∞" : incumbent());
+        str += "\n\tgap: " + (Double.isInfinite(gap()) ? "∞" : incumbent());
+        str += "\n";
+        return str;
     }
 }
-
