9b1ae24e27743567d30932516cf4b5dbab7eed64
qme <quentin.meurisse@cetic.be>
2026-07-01 10:54:21 +0200

    update solution printer

--- src/main/java/org/ddolib/examples/nolayer/tsptw/TspTwAwAstarMain.java
+++ src/main/java/org/ddolib/examples/nolayer/tsptw/TspTwAwAstarMain.java
@@ -6,6 +6,7 @@ import org.ddolib.modeling.nolayer.AwAstarModel;
 import org.ddolib.modeling.nolayer.FastLowerBound;
 import org.ddolib.modeling.nolayer.Problem;
 import org.ddolib.solving.awastar.core.solver.nolayer.AwAstarSolver;
+import org.ddolib.util.io.SolutionPrinter;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -39,7 +40,8 @@ public class TspTwAwAstarMain {
         };
         var solver = new AwAstarSolver<>(model);
 
-        Solution bestSolution = solver.minimize(searchStatistics -> false, (sol, stat) -> {
+        Solution bestSolution = solver.minimize(searchStatistics -> searchStatistics.runtime() > 1000, (sol, stat) -> {
+            SolutionPrinter.printSolution(stat, sol);
         });
 
         System.out.println(bestSolution.statistics());
