cc24472377433d3917e1abdca2cb369e337ba32f
qme <quentin.meurisse@cetic.be>
2026-07-01 10:25:23 +0200

    add dominance rule for tsptw

--- src/main/java/org/ddolib/examples/nolayer/tsptw/TspTwAwAstarMain.java
+++ src/main/java/org/ddolib/examples/nolayer/tsptw/TspTwAwAstarMain.java
@@ -1,5 +1,6 @@
 package org.ddolib.examples.nolayer.tsptw;
 
+import org.ddolib.common.dominance.NoLayerDominanceChecker;
 import org.ddolib.common.solver.nolayer.Solution;
 import org.ddolib.modeling.nolayer.AwAstarModel;
 import org.ddolib.modeling.nolayer.FastLowerBound;
@@ -12,7 +13,7 @@ import java.nio.file.Path;
 public class TspTwAwAstarMain {
 
     public static void main(String[] args) throws IOException {
-        final String instance = args.length == 0 ? Path.of("data", "TSPTW", "AFG", "rbg010a.tw").toString() : args[0];
+        final String instance = args.length == 0 ? Path.of("data", "TSPTW", "AFG", "rbg172a.tw").toString() : args[0];
         final TSPTWProblem problem = TSPTWProblem.fromFile(instance);
 
         var model = new AwAstarModel<TSPTWState>() {
@@ -30,11 +31,18 @@ public class TspTwAwAstarMain {
             public FastLowerBound<TSPTWState> lowerBound() {
                 return new TSPTWFbl(problem);
             }
+
+            @Override
+            public NoLayerDominanceChecker<TSPTWState> dominance() {
+                return new TSPTWDominance();
+            }
         };
         var solver = new AwAstarSolver<>(model);
 
         Solution bestSolution = solver.minimize(searchStatistics -> false, (sol, stat) -> {
         });
+
+        System.out.println(bestSolution.statistics());
         System.out.println(bestSolution);
 
 
